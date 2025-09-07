ac05416089e072c0b266ca75115029f0cc596982
Dyavar Naveen Kumar <dyavarnaveen8247@gmail.com>
2025-09-07 14:26:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-  origin: "https://customer-managemrnt-app-client.vercel.app/", 
+  origin: "https://customer-managemrnt-app-client.vercel.app", 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
