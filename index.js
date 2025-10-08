0b68c62ba0058fe5762c04ebf363939fd7ff2490
Dyavar Naveen Kumar <dyavarnaveen8247@gmail.com>
2025-10-08 22:51:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -225,7 +225,7 @@ app.put("/api/profile", authMiddleware, (req, res) => {
 
 
 // Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
